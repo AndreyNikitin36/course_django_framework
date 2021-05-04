6b724654187e5b610d5adaf0bf2f0dac6085eb8b
yugin30 <nassa1983@mail.ru>
2021-05-04 21:03:43 +0500

    HW_lesson_2

---
+++ geekshopNAS/mainapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import main, products, contact
+
+
+urlpatterns = [
+   path('', main),
+   path('products/', products),
+   path('contact/', contact),
+]
+
