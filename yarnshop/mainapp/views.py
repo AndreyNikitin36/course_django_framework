309582ca0a515d444850d4d6d9f5b2b496f954c6
yugin30 <nassa1983@mail.ru>
2021-05-11 21:37:19 +0500

    HW_lesson_4

---
+++ yarnshop/mainapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import ProductCategory, Product
+
+
+def products(request, pk=None):
+    print(pk)
+    links_menu = {'links': [
+        {'href': 'index', 'name': 'все'},
+        {'href': 'index', 'name': 'дом'},
+        {'href': 'index', 'name': 'офис'},
+        {'href': 'index', 'name': 'классика'},
+        {'href': 'index', 'name': 'модерн'},
+    ]}
+
+    return render(request, 'mainapp/products.html', context=links_menu)
+
+
+
